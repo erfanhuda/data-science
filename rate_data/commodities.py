2170cb9b86a59cf9ce1f0b7d08dc59ab304046e0
mandauhitam <42535422+mandauhitam@users.noreply.github.com>
2020-05-06 09:14:43 +0700

    commiting the changes in commodities

--- rate_data/commodities.py
+++ rate_data/commodities.py
@@ -2,9 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import date, datetime
+import schedule
+import time
+
+## Created by Muhammad Erfan Huda
+## Follow on GitHub : @mandauhitam
 
 today = date.today()
-time = str(datetime.time(datetime.now()))
+tm = str(datetime.time(datetime.now()))
 
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'}
 
@@ -19,6 +24,10 @@ def gold_silver():
     df = pd.DataFrame(columns=rows)
     for i in range(1, len(rows)):
         df = df.append(pd.Series(columns, index=rows), ignore_index=True)
-        print(df, today, time)
+        print(df, str(datetime.now()))
 
-gold_silver()
+schedule.every(1).hours.do(gold_silver)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
--- rate_data/rate_idr.py
+++ rate_data/rate_idr.py
@@ -2,9 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import date
+import schedule
+import time
 
 ## Created by Muhammad Erfan Huda
-## Followed git on @mandauhitam
+## Follow on GitHub : @mandauhitam
 
 today = date.today()
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'}
@@ -31,9 +33,7 @@ def rate_bi():
         df = df.append(pd.Series(values, index=columns), ignore_index=True)
         print(df, fdate)
 
-        df.to_excel(str(today)+'rate_bi.xlsx', index=False)
-
-rate_bi()
+        # df.to_excel(str(today)+'rate_bi.xlsx', index=False)
 
 def rate_pajak():
 
@@ -60,4 +60,9 @@ def rate_pajak():
 
         df.to_excel(fdate.text.replace('\n\t\t\t\t\t\t', '')+'rate_pajak.xlsx', index=False)
 
-rate_pajak()
+schedule.every().day.at('02:29').do(rate_bi)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+    schedule.clear()
